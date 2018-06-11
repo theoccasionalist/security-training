ef1c2dbb0e022b4617f9753e786ac3d3f4de52ed
theoccasionalist <mrmccall07@gmail.com>
2018-06-11 19:35:37 -0400

    in-memory authentication

--- guest-app/src/main/java/com/frankmoley/security/app/ApplicationSecurityConfiguration.java
+++ guest-app/src/main/java/com/frankmoley/security/app/ApplicationSecurityConfiguration.java
@@ -1,9 +1,17 @@
 package com.frankmoley.security.app;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
 @EnableWebSecurity
@@ -19,5 +27,15 @@ public class ApplicationSecurityConfiguration extends WebSecurityConfigurerAdapt
 			.and()
 			.httpBasic();
 	}
+	
+	@Bean
+	@Override
+	public UserDetailsService userDetailsService() {
+		List<UserDetails> users = new ArrayList<>();
+		users.add(User.withDefaultPasswordEncoder().username("phil").password("password").roles("USER", "ADMIN").build());
+		users.add(User.withDefaultPasswordEncoder().username("bill").password("anotherPassword").roles("USER").build());
+		return new InMemoryUserDetailsManager(users);
+	}
+	
 
 }
